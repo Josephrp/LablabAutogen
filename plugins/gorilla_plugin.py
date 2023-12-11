233a5803cd0560a07d0c16572e01ce16e2d8e242
yikes <111034762+twilwa@users.noreply.github.com>
2023-12-10 21:05:59 -0800

    aider: Refactored GorillaPlugin to collect environment information before and after executing commands.
    # Aider chat conversation:
    
    USER: hmm, is there a way we can direct the plugin to collect information about the environment the commands are being run in, so it doesn't need to always/only rely on input from the user?
    
    ASSISTANT: Certainly! To collect information about the environment in which the commands are being run, we can enhance the `GorillaPlugin` to gather system information before and after executing commands. This can be done by executing additional commands that retrieve system information, such as `uname` on Unix-like systems or `systeminfo` on Windows.
    
    Here's how we can modify the `plugins/gorilla_plugin.py` file to include environment data collection:
    
    1. Add a method to gather system information.
    2. Call this method before and after executing the CLI commands to collect the environment information.
    
    Let's implement these changes:
    
    plugins/gorilla_plugin.py
    ```python
    <<<<<<< SEARCH
    import asyncio
    import asyncio.subprocess as subprocess
    from typing import List
    =======
    import asyncio
    import asyncio.subprocess as subprocess
    from typing import List, Dict
    >>>>>>> REPLACE
    ```
    
    plugins/gorilla_plugin.py
    ```python
    <<<<<<< SEARCH
    class GorillaPlugin:
        _cli_path: str
    
        def __init__(self, cli_path: str):
            self._cli_path = cli_path
    =======
    class GorillaPlugin:
        _cli_path: str
        _env_info: Dict[str, str]
    
        def __init__(self, cli_path: str):
            self._cli_path = cli_path
            self._env_info = {}
    >>>>>>> REPLACE
    ```
    
    plugins/gorilla_plugin.py
    ```python
    <<<<<<< SEARCH
        async def execute_commands(self, cli_commands: List[str]):
            """
            Execute the generated CLI commands and provide an interface for user feedback.
            """
    =======
        async def collect_environment_info(self) -> None:
            """
            Collects information about the environment where the commands are executed.
            """
            uname_command = "uname -a"  # This is for Unix-like systems, for Windows use 'systeminfo'
            process = await subprocess.create_subprocess_shell(
                uname_command,
                stdout=subprocess.PIPE,
                stderr=subprocess.PIPE
            )
            stdout, stderr = await process.communicate()
    
            if process.returncode == 0:
                self._env_info['uname'] = stdout.decode().strip()
            else:
                self._env_info['uname'] = f"Error collecting environment info: {stderr.decode().strip()}"
    
        async def execute_commands(self, cli_commands: List[str]):
            # Collect initial environment info
            await self.collect_environment_info()
            initial_env_info = self._env_info.copy()
    
    >>>>>>> REPLACE
    ```
    
    plugins/gorilla_plugin.py
    ```python
    <<<<<<< SEARCH
                    # After execution, get user feedback
                    user_feedback = input("Please provide feedback about the environment: ")
                    print(f"Received feedback: {user_feedback}")
    =======
                    # Collect updated environment info
                    await self.collect_environment_info()
                    updated_env_info = self._env_info.copy()
    
                    # Compare initial and updated environment info, if needed
                    # ...
    
                    # After execution, get user feedback
                    user_feedback = input("Please provide feedback about the environment: ")
                    print(f"Received feedback: {user_feedback}")
                    print(f"Initial environment info: {initial_env_info}")
                    print(f"Updated environment info: {updated_env_info}")
    >>>>>>> REPLACE
    ```
    
    These changes will allow the `GorillaPlugin` to collect system information before and after executing each command. The `collect_environment_info` method can be expanded to collect more detailed information as required. The initial and updated environment information can be compared to detect any changes that occurred as a result of the command execution.

--- plugins/gorilla_plugin.py
+++ plugins/gorilla_plugin.py
@@ -1,12 +1,14 @@
 import asyncio
 import asyncio.subprocess as subprocess
-from typing import List
+from typing import List, Dict
 
 class GorillaPlugin:
     _cli_path: str
+    _env_info: Dict[str, str]
 
     def __init__(self, cli_path: str):
         self._cli_path = cli_path
+        self._env_info = {}
     """
     A plugin that uses the Gorilla CLI to perform a series of executions based on a natural language query or high level overview of the user's problem.
     """
@@ -33,10 +35,27 @@ class GorillaPlugin:
         cli_commands = ["echo 'Command 1'", "echo 'Command 2'"]  # Example commands
         return cli_commands
 
+    async def collect_environment_info(self) -> None:
+        """
+        Collects information about the environment where the commands are executed.
+        """
+        uname_command = "uname -a"  # This is for Unix-like systems, for Windows use 'systeminfo'
+        process = await subprocess.create_subprocess_shell(
+            uname_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        stdout, stderr = await process.communicate()
+
+        if process.returncode == 0:
+            self._env_info['uname'] = stdout.decode().strip()
+        else:
+            self._env_info['uname'] = f"Error collecting environment info: {stderr.decode().strip()}"
+
     async def execute_commands(self, cli_commands: List[str]):
-        """
-        Execute the generated CLI commands and provide an interface for user feedback.
-        """
+        # Collect initial environment info
+        await self.collect_environment_info()
+        initial_env_info = self._env_info.copy()
         for command in cli_commands:
             user_confirmation = input(f"Do you want to execute: {command}? (yes/no) ")
             if user_confirmation.lower() == 'yes':
@@ -55,9 +74,18 @@ class GorillaPlugin:
                     print(f"Command failed: {command}")
                     print(f"Error: {stderr.decode().strip()}")
 
+                # Collect updated environment info
+                await self.collect_environment_info()
+                updated_env_info = self._env_info.copy()
+
+                # Compare initial and updated environment info, if needed
+                # ...
+
                 # After execution, get user feedback
                 user_feedback = input("Please provide feedback about the environment: ")
                 print(f"Received feedback: {user_feedback}")
+                print(f"Initial environment info: {initial_env_info}")
+                print(f"Updated environment info: {updated_env_info}")
             else:
                 print("Command execution skipped by user.")
 
